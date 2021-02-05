2e2e64b6d5d3975947ea42b639f15f71bd094dc7
Maksim <makc.pinsk@gmail.com>
2021-02-05 14:32:37 +0300

    add readme

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://admin:admin@localhost:3306/marvel'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/marvel'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
